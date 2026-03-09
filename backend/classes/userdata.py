fad8edc827625764e2dfbcd2726d59a89ec59598
Baybhin Gurung <baybhin+github@gmail.com>
2026-03-08 23:18:42 -0400

    Add protection checks to the fields

--- backend/classes/userdata.py
+++ backend/classes/userdata.py
@@ -1,11 +1,12 @@
-from pydantic import BaseModel 
+from pydantic import BaseModel, Field
 
 class LoginData(BaseModel):
-    email: str 
-    password: str 
+    email: str = Field(min_length=10, max_length=50)
+    password: str = Field(min_length=8, max_length=20)
 
 class SignUpData(BaseModel):
-    email: str 
-    password: str 
-    age: int 
-    # More fields to be determined 
+    firstname: str = Field(min_length=1, max_length=100)
+    lastname: str = Field(min_length=1, max_length=100)
+    email: str = Field(min_length=10, max_length=50)
+    password: str = Field(min_length=8, max_length=20)
+
