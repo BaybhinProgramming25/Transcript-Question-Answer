0fddb1d69b32afa033fd84bc51207eb720a10677
Baybhin Gurung <baybhin+github@gmail.com>
2026-03-31 19:40:32 -0400

    Update table base model

--- backend/classes/userdata.py
+++ backend/classes/userdata.py
@@ -1,12 +1,12 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, EmailStr
 
 class LoginData(BaseModel):
-    email: str = Field(min_length=10, max_length=50)
+    email: EmailStr = Field(min_length=10, max_length=50)
     password: str = Field(min_length=8, max_length=20)
 
 class SignUpData(BaseModel):
     firstname: str = Field(min_length=1, max_length=100)
     lastname: str = Field(min_length=1, max_length=100)
-    email: str = Field(min_length=10, max_length=50)
+    email: EmailStr = Field(min_length=10, max_length=50)
     password: str = Field(min_length=8, max_length=20)
 
