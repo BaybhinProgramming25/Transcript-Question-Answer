6b4e554c57d650450f03ea1b420c9175a5e7386c
Baybhin Gurung <baybhin+github@gmail.com>
2026-04-13 13:00:49 -0400

    Add pool pre ping and pool recycle to ensure no idle connections are kept

--- backend/database/database.py
+++ backend/database/database.py
@@ -6,7 +6,7 @@ load_dotenv(interpolate=True, override=True)
 import os 
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True, pool_recycle=3600)
 SessionLocal = sessionmaker(bind=engine)
 
 class Base(DeclarativeBase):
